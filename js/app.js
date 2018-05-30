a97c32c36185d71062c9efce6e1705fa6ca94f05
Jake-Tibbits <jake@bonusmedia.co.uk>
2018-05-30 14:58:44 +0100

    create Character parent class and extending subclasses for ES6 functionality

--- js/app.js
+++ js/app.js
@@ -1,88 +1,77 @@
-// Enemies our player must avoid
-var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    // initial x position
+// Enemies and Characters share features - let's make a parent Class they can extend from
+class Character {
+  constructor(sprite, x, y, speed) {
+    // character image
+    this.sprite = sprite;
+    // horizontal position
     this.x = x;
-    // initial y position
-    this.y = y;
-    //
-    this.speed = 100;
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
-    if(this.x > 505){
-      this.x = -70;
-    }
-};
-
-// Draw the enemy on the screen
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y) {
-    //player-character image
-    this.sprite = 'images/char-boy.png';
-    // initial x position
-    this.x = x;
-    // initial y position
+    // vertical position
     this.y = y;
     // movement speed
-    this.speed = 50;
-};
-// Update the player's position
-Player.prototype.update = function(moveX, moveY) {
-    if(moveX){ this.x = (this.x + moveX); }
-    if(moveY){ this.y = (this.y + moveY); }
-};
-// Draw the player on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-// Handle player input
-Player.prototype.handleInput = function(key) {
-
-  var moveX = 0;
-  var moveY = 0;
-
-  if(key == 'left'){
-    moveX = -this.speed;
-  } else if(key == 'right'){
-    moveX = this.speed;
-  } else if(key == 'up'){
-    moveY = -this.speed;
-  } else if(key == 'down'){
-    moveY= this.speed;
+    this.speed = speed;
   }
+  //render the character image on the game canvas
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+}
 
-  player.update(moveX, moveY);
+// Enemies our player must avoid
+class Enemy extends Character {
+  constructor(sprite, x, y, speed){
+    super(sprite, x, y, speed);
+  }
+  update(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+      this.x += this.speed * dt;
+      if(this.x > 505){
+        this.x = -70;
+      }
+  };
+}
 
-};
+// Player Character
+class Player extends Character {
+  constructor(sprite, x, y, speed){
+    super(sprite, x, y, speed);
+    // Update the player's position
+  }
+  update(moveX, moveY) {
+      if(moveX){ this.x = (this.x + moveX); }
+      if(moveY){ this.y = (this.y + moveY); }
+  };
+  handleInput(key) {
+
+    let moveX = 0;
+    let moveY = 0;
+
+    if(key == 'left'){
+      moveX = -this.speed;
+    } else if(key == 'right'){
+      moveX = this.speed;
+    } else if(key == 'up'){
+      moveY = -this.speed;
+    } else if(key == 'down'){
+      moveY= this.speed;
+    }
+
+    player.update(moveX, moveY);
+
+  };
+}
 
 
 
 // Now instantiate your objects.
 // Place the player object in a variable called player
-var player = new Player(220, 450);
+const player = new Player('images/char-boy.png', 220, 450, 50);
 
-var enemy1 = new Enemy(0,50);
+const enemy1 = new Enemy('images/enemy-bug.png', -50, 100, 100);
 
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [enemy1];
+const allEnemies = [enemy1];
 
 
 
@@ -91,7 +80,7 @@ var allEnemies = [enemy1];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
